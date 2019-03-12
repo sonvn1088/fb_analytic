f47b1e97da787760332205365a1f329e991d10cb
Son Nguyen <sonvn1088@gmail.com>
2019-03-12 17:55:41 +0700

    export links

---
+++ app/Console/Commands/ExportTopLinks.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Helps\Links;
+use Illuminate\Support\Facades\Storage;
+
+class ExportTopLinks extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'export:topLinks';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Export top link to file';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $links = Links::getTopLinks();
+        Storage::put('public/links.txt', json_encode($links));
+    }
+}
