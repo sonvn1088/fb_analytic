dd5e507b024088d86f6e42cbb3c73eb6f489701d
Son Nguyen <sonvn1088@gmail.com>
2019-04-11 16:20:29 +0700

    manage accounts & pages

--- app/Helps/Facebook.php
+++ app/Helps/Facebook.php
@@ -153,9 +153,7 @@ class Facebook
         $publishedPosts = self::getPublishedPosts($token, time() - 24*3600);
 
         $postedUris = self::deleteDuplicatedPosts($token, $scheduledPosts, $publishedPosts);
-        self::reschedule_posts($token, $stepTime, $scheduledPosts);
-
-        $firstScheduledTime = self::getNextScheduledTime($scheduledPosts, $stepTime);
+        $firstScheduledTime = self::getNextScheduledTime($token, $scheduledPosts, $stepTime);
 
         $i = 0;
         foreach($posts as $id => $post){
@@ -195,7 +193,9 @@ class Facebook
      *
      * @return int
      */
-    static function getNextScheduledTime($scheduledPosts, $stepTime){
+    static function getNextScheduledTime($token, $scheduledPosts, $stepTime){
+        self::reschedulePosts($token, $scheduledPosts, $stepTime);
+
         $current_time = floor(time()/300)*300+300; //round to 5 mins
 
         if(empty($scheduledPosts))
@@ -218,11 +218,11 @@ class Facebook
      * @param $page_token string
      * @param $step_time int
      */
-    static public function reschedule_posts($token, $stepTime, $scheduledPosts = null){
+    static public function reschedulePosts($token, $scheduledPosts = null, $stepTime){
         if(!$scheduledPosts)
             $scheduledPosts = self::getScheduledPosts($token);
 
-        if(count($scheduledPosts) > 1){
+        if(count($scheduledPosts) > 0){
             $rootTime = floor(time()/300)*300+300; //round to 5 mins
             $is_current = true;
             foreach($scheduledPosts as $postId => $scheduledPost){
@@ -238,14 +238,15 @@ class Facebook
             $i = 0;
             foreach($scheduledPosts as $postId => $scheduledPost){
                 if($scheduledPost['scheduled_publish_time'] > time()){
-                    if( $scheduledPost['scheduled_publish_time'] != $rootTime+$i*$stepTime*60){
-                        $time = $rootTime+$i*$stepTime*60;
+                    $time = $rootTime+$i*$stepTime*60;
+                    if( $scheduledPost['scheduled_publish_time'] != $time){
                         self::post($postId, ['scheduled_publish_time' => $time], $token);
                     }
                     $i++;
                 }
             }
 
+            return $rootTime+$i*$stepTime*60;
             /*foreach($scheduledPosts as $postId => $scheduledPost){
                 if($scheduledPost['scheduled_publish_time'] < time() - 5*60){
                     $time = ($is_current && $i == 0)?$rootTime+10*60:$rootTime+$i*$stepTime*60;
