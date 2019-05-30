ea6a7605a463d7e21a196af7d76b951c459a5357
Son Nguyen <sonvn1088@gmail.com>
2019-05-30 17:05:16 +0700

    manage accounts & pages

--- app/Helps/Links.php
+++ app/Helps/Links.php
@@ -50,7 +50,8 @@ class Links
             if(!$ignored){
                 if(!$link->content){
                     $article = General::parseArticle($link->url, $type);
-                    $link->title = Arr::get($article, 'title');
+                    if(Arr::get($article, 'title'))
+                        $link->title = Arr::get($article, 'title');
                     $link->excerpt = Arr::get($article, 'excerpt');
                     $link->content = Arr::get($article, 'content');
                     $link->save();
